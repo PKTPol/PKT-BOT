ac6307fb8ca6121a7cb0e391ba77b8c350783af9
PKT | Pol <93952466+PKTPol@users.noreply.github.com>
2021-11-09 19:19:52 +0100

    Nueva descripción en ohelp
    Ahora sale bien explicado!

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ client.on("ready", () => {console.log(`Logged in as ${client.user.tag}!`)
 
 
 client.on("message", msg => {
-  if (msg.content === "help") {
-    msg.reply("Para ayuda haz esto")
+  if (msg.content === "ohelp") {
+    msg.reply("Hola! soy Oliv Beta Bot, estoy aqui para que me probeis antes de que empieze a funcionar en el servidor de Oliv, por favor reportar qualquier error que detecteis y sugerir cosas que pueda hacer! Os dejo la lista de comandos que tengo:  ohelp todos mis comandos ohello digo ola  ")
  }
 })
 client.on("message", msg => {
